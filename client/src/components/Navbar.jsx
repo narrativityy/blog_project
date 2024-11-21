61ad158cf27078ff117e6d8137f494f2e7c57b5b
narrativityy <jake_crowley@comcast.net>
2024-11-21 14:34:19 -0800

    corrected logout funtion

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+import Axios from 'axios'
 
 const Navbar = () => {
 
-  const handleLogout = () => {
-    Cookies.remove('userId')
-    Cookies.remove('username')
-    window.location.href = '/'
-  }
+  const navigate = useNavigate()
+
+  const logoutHandler = () => {
+    Axios.post('http://localhost:8001/api/users/logout', {}, {withCredentials: true})
+      .then(res => {
+        navigate('/')
+        window.location.reload();
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
 
   return (
     <div className='flex justify-between items-center mb-4'>
-      <h3 className="text-xl font-bold"><Link to='/'>Blog</Link></h3>
+      <h3 className="text-xl font-bold transition-transform duration-300 hover:scale-105"><Link to='/'>Blog</Link></h3>
       <div className='flex gap-4'>
-        <h5 className="text-md"><Link to='/post/create'>Create Post</Link></h5>
-        <h5 className="text-md"><Link to='/profile'>{Cookies.get('username')}</Link></h5>
-        <h5 className="text-md cursor-pointer" onClick={handleLogout}>Logout</h5>
+        <h5 className="text-md transition-transform duration-300 hover:scale-105"><Link to='/post/create'>Create Post</Link></h5>
+        <h5 className="text-md transition-transform duration-300 hover:scale-105"><Link to='/profile'>{Cookies.get('username')}</Link></h5>
+        <h5 className="text-md cursor-pointer transition-transform duration-300 hover:scale-105" onClick={logoutHandler}>Logout</h5>
       </div>
     </div>
   )
