25b1ee981495c0f2e296f59112f27220c6498fc9
narrativityy <jake_crowley@comcast.net>
2024-11-21 14:29:09 -0800

    added create post route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
-import App from './App.jsx'
 import Login from './components/Login.jsx'
 import Register from './components/Register.jsx'
 import Dashboard from './components/Dashboard.jsx'
 import Profile from './components/Profile.jsx'
+import CreatePost from './components/CreatePost.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -17,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/login" element={Cookies.get('userId') ? <Navigate to="/dashboard" /> : <Login />} />
         <Route path='/dashboard' element={Cookies.get('userId') ? <Dashboard /> : <Navigate to="/login" />} />
         <Route path='/profile' element={Cookies.get('userId') ? <Profile /> : <Navigate to="/login" />} />
+        <Route path='/post/create' element={Cookies.get('userId') ? <CreatePost /> : <Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
