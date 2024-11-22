c618568fa19753b70c64f326b40c7442c764a271
narrativityy <jake_crowley@comcast.net>
2024-11-22 12:16:03 -0800

    removed text animations when in mobile view

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,24 +24,27 @@ const Navbar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  // Determine if the screen is small
+  const isSmallScreen = window.innerWidth < 768;
+
   return (
     <nav className="bg-teal-700 shadow-md rounded">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <h3 className="text-xl font-bold text-white transition-transform duration-300 hover:scale-105">
+            <h3 className={`text-xl font-bold text-white ${!isSmallScreen ? 'transition-transform duration-300 hover:scale-105' : ''}`}>
               <Link to="/">Blog</Link>
             </h3>
           </div>
           <div className="hidden md:flex gap-4">
-            <Link to="/post/create" className="text-md text-white transition-transform duration-300 hover:scale-105">
+            <Link to="/post/create" className={`text-md text-white ${!isSmallScreen ? 'transition-transform duration-300 hover:scale-105' : ''}`}>
               Create Post
             </Link>
-            <Link to="/profile" className="text-md text-white transition-transform duration-300 hover:scale-105">
+            <Link to="/profile" className={`text-md text-white ${!isSmallScreen ? 'transition-transform duration-300 hover:scale-105' : ''}`}>
               {Cookies.get('username')}
             </Link>
             <span
-              className="text-md text-white cursor-pointer transition-transform duration-300 hover:scale-105"
+              className={`text-md text-white cursor-pointer ${!isSmallScreen ? 'transition-transform duration-300 hover:scale-105' : ''}`}
               onClick={logoutHandler}
             >
               Logout
@@ -57,14 +60,14 @@ const Navbar = () => {
       {isMobileMenuOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link to="/post/create" className="block text-md text-white transition-transform duration-300 hover:scale-105">
+            <Link to="/post/create" className="block text-md text-white">
               Create Post
             </Link>
-            <Link to="/profile" className="block text-md text-white transition-transform duration-300 hover:scale-105">
+            <Link to="/profile" className="block text-md text-white">
               {Cookies.get('username')}
             </Link>
             <span
-              className="block text-md text-white cursor-pointer transition-transform duration-300 hover:scale-105"
+              className="block text-md text-white cursor-pointer"
               onClick={logoutHandler}
             >
               Logout
