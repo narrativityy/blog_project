db838c027d5bc8982127e046c756c6a11fe4cba6
narrativityy <jake_crowley@comcast.net>
2024-11-22 12:14:23 -0800

    added likePost function

--- server/controllers/post-controller.js
+++ server/controllers/post-controller.js
@@ -28,6 +28,8 @@ module.exports.createNewPost = (req, res) => {
                 return res.status(400).json({ message: "User not found" });
             }
             req.body.user = user
+            req.body.likes = []
+            req.body.comments = []
             Post.create(req.body)
                 .then(newlyCreatedPost => {
                     res.json(newlyCreatedPost)
@@ -63,3 +65,33 @@ module.exports.deleteAnExistingPost = (req, res) => {
         .catch((err) => {
             res.json(err)
         });}
+
+module.exports.likePost = (req, res) => {
+  Post.findOne({ _id: req.body.postId })
+    .then(post => {
+      if (!post) {
+        return res.status(400).json({ message: "Post not found" });
+      }
+
+      const usernameIndex = post.likes.indexOf(req.body.username);
+
+      if (usernameIndex === -1) {
+        // User has not liked the post yet, so add the like
+        post.likes.push(req.body.username);
+      } else {
+        // User has already liked the post, so remove the like
+        post.likes.splice(usernameIndex, 1);
+      }
+
+      post.save()
+        .then(updatedPost => {
+          res.json(updatedPost);
+        })
+        .catch(err => {
+          res.status(500).json({ message: "Error saving post", error: err });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error finding post", error: err });
+    });
+}
